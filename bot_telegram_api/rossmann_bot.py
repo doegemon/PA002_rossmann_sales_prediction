bab51bc346b64725e0433603bf519282e47ceb5f
Nicolas Doege <nicolasldoege@gmail.com>
2022-11-21 17:43:47 -0300

    updating api url

--- bot_telegram_api/rossmann_bot.py
+++ bot_telegram_api/rossmann_bot.py
@@ -55,7 +55,7 @@ def load_data(store_number):
 def predict(data):
 
   # Request na API
-  url = 'https://rossmann-telegram-bot.onrender.com/rossmann/predict'
+  url = 'https://rossmann-handler-api.onrender.com/rossmann/predict'
   header = {'Content-type': 'application/json'}
   data = data
 
